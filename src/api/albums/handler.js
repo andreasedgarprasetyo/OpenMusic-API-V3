0164dc208195c92e2bc3dabb49a9b4ddc09e6c40
Andreasedgarp01 <andreasedgarp@gmail.com>
2022-05-09 00:59:11 +0700

    try to fix upload

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -10,6 +10,8 @@ class AlbumsHandler {
     this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
     this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
     this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
+    this.postUploadAlbumCoverHandler =
+      this.postUploadAlbumCoverHandler.bind(this);
   }
 
   async postAlbumHandler(request, h) {
@@ -140,6 +142,50 @@ class AlbumsHandler {
         return response;
       }
 
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async postUploadAlbumCoverHandler(request, h) {
+    try {
+      const { coverUrl } = request.payload;
+      const { id } = request.params;
+      this._validator.validateImageHeaders(coverUrl.hapi.headers);
+
+      const filename = await this._storageService.writeFile(
+        coverUrl,
+        cover.hapi
+      );
+
+      const fileLocation = `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`;
+
+      await this._service.editAlbumCover(id, fileLocation);
+
+      const response = h.response({
+        status: "success",
+        message: "Cover album berhasil disimpan",
+        data: {
+          fileLocation,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
       // Server ERROR!
       const response = h.response({
         status: "error",
